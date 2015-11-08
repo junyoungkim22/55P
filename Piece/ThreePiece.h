fa97d9a4a3fcf2b5869f12bb4b0ea2bb1a3f9c97
pkw1012 <pkw1012@postech.ac.kr>
2015-11-08 19:27:28 +0900

    Create ThreePiece.h

---
+++ Piece/ThreePiece.h
@@ -0,0 +1,13 @@
+#ifndef THREEPIECE_H
+#define THREEPIECE_H
+
+#include "piece.h"
+
+class ThreePiece : public Piece
+{
+public:
+    ThreePiece(int HP, int position, int team);
+};
+
+#endif // THREEPIECE_H
+
