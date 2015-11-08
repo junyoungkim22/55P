c361260e30b4e5bfa0df49a6e4ae6947057bd025
junyoungkim22 <junyoung2@gmail.com>
2015-11-08 20:13:31 +0900

    Update Board.cpp
    12번 째 칸을 청암이라고 가정하고, 청암에 도착하면 메세지가 뜨도록 설정

--- Board/Board.cpp
+++ Board/Board.cpp
@@ -2,10 +2,12 @@
 
 Board::Board()
 {
-	for(int i = 0; i < SPACE_NUM; i++)
+	for(int i = 0; i < SPACE_NUM - 1; i++)
 	{
 		this->BoardSpaces[i] = new Space;
 	}
+	this->BoardSpaces[SPACE_NUM - 1] = new BuildingSpace;
+	this->BoardSpaces[SPACE_NUM - 1]->setExplainString("Library\n");
 }
 
 Board::~Board()
@@ -18,10 +20,12 @@ Board::~Board()
 
 void Board::move(int index, int moveNum)
 {
+	//경원이가 piece가 move하는 것을 이미 구현함
 	int current = this->playerPieces[index].getPosition();
 	this->playerPieces[index].setPosition((current + moveNum) % SPACE_NUM);
 }
 
+//draw current board state
 void Board::draw()
 {
 	std::cout << std::string(70, '\n');
@@ -30,10 +34,18 @@ void Board::draw()
 		std::cout << "Space " << i << "   ";
 		for(int j = 0; j < PIECE_NUM; j++)
 		{
+			//show piece location
 			if(this->playerPieces[j].getPosition() == i)
 			{
 				std::cout << "piece " << j << " is here.   ";
+				
+				//if piece is on a buildling, explain building
+				if(this->BoardSpaces[i]->getSpaceType() == "Building")
+				{
+					this->BoardSpaces[i]->explain();
+				}
 			}
 		}
-		std::cout << "\n"; }
+		std::cout << "\n"; 
+	}
 }
