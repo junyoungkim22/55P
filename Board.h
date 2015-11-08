e45389d74c989949275460eb948774ec26a8388f
junyoungkim22 <junyoung2@gmail.com>
2015-11-08 18:22:26 +0900

    Create Board.h
    The header file for the Board class

---
+++ Board.h
@@ -0,0 +1,28 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include<iostream>
+
+#include "Piece.h"
+#include "Space.h"
+
+#define SPACE_NUM 13
+#define PIECE_NUM 1
+
+class Board
+{
+private:
+	Space* BoardSpaces[SPACE_NUM];
+	Piece playerPieces[PIECE_NUM];
+
+public:
+	Board();
+	~Board();
+
+	void move(int index, int moveNum);
+	void draw();
+
+
+};
+
+#endif
