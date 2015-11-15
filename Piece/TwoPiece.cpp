d5f97b9a9ea76147f0372baf2324d388c1f3cca3
pkw1012 <pkw1012@postech.ac.kr>
2015-11-15 21:15:05 +0900

    Update TwoPiece.cpp

--- Piece/TwoPiece.cpp
+++ Piece/TwoPiece.cpp
@@ -1,6 +1,6 @@
 #include "twopiece.h"
 
-TwoPiece::TwoPiece(int HP, int position, int team) : Piece(HP, position, team)
+TwoPiece::TwoPiece(int HP, int position, int team, int num) : Piece(HP, position, team, num)
 {
     setMaxHP(200);
 }
