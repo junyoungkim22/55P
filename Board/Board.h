d1fe03ff8359b2de5c92f2cde5404c1fa848d37e
junyoungkim22 <junyoung2@gmail.com>
2015-11-15 21:13:58 +0900

    Update Board.h

--- Board/Board.h
+++ Board/Board.h
@@ -6,21 +6,23 @@
 #include "Piece.h"
 #include "Space.h"
 #include "BuildingSpace.h"
+#include "HealSpace.h"
 
-#define SPACE_NUM 13
+#define SPACE_NUM 29
 #define PIECE_NUM 1
 
 class Board
 {
 private:
 	Space* BoardSpaces[SPACE_NUM];
-	Piece playerPieces[PIECE_NUM];
+	Piece* playerPieces[PIECE_NUM];
 
 public:
 	Board();
 	~Board();
 
 	void move(int index, int moveNum);
+	void update();
 	void draw();
 
 
