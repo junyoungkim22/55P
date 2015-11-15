8a4b6ef11df17399d1742aa9dd42f0cc4e642487
pkw1012 <pkw1012@postech.ac.kr>
2015-11-15 21:13:50 +0900

    Update PieceList.h

--- Piece/PieceList.h
+++ Piece/PieceList.h
@@ -6,13 +6,14 @@
 class PieceList
 {
 private:
-    Piece *piece[6];
+    Piece *team1[3], *team2[3];
 public:
     PieceList();
-    void turn_action(int piece_num, int move);
-    void capture(int captured_piece);
-    void unite(int p1, int p2);
-    void die(int p);
+    void turn_action(int team, int piece_num, int move_num);
+    void capture(int captured_piece, int team);
+    void unite(int p1, int p2, int team);
+    void die(int piece_num, int team);
 };
 
 #endif // PIECELIST_H
+
