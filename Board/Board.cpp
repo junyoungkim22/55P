1302f0c7840d017f84fe03d019bb6dc9fd780cbd
junyoungkim22 <junyoung2@gmail.com>
2015-11-15 21:13:30 +0900

    Update Board.cpp

--- Board/Board.cpp
+++ Board/Board.cpp
@@ -1,51 +1,31 @@
-#include "Board.h"
+#ifndef BOARD_H
+#define BOARD_H
 
-Board::Board()
-{
-	for(int i = 0; i < SPACE_NUM - 1; i++)
-	{
-		this->BoardSpaces[i] = new Space;
-	}
-	this->BoardSpaces[SPACE_NUM - 1] = new BuildingSpace;
-	this->BoardSpaces[SPACE_NUM - 1]->setExplainString("Library\n");
-}
+#include<iostream>
 
-Board::~Board()
-{
-	for(int i = 0; i < SPACE_NUM; i++)
-	{
-		delete this->BoardSpaces[i];
-	}
-}
+#include "Piece.h"
+#include "Space.h"
+#include "BuildingSpace.h"
+#include "HealSpace.h"
 
-void Board::move(int index, int moveNum)
-{
-	//경원이가 piece가 move하는 것을 이미 구현함
-	int current = this->playerPieces[index].getPosition();
-	this->playerPieces[index].setPosition((current + moveNum) % SPACE_NUM);
-}
+#define SPACE_NUM 29
+#define PIECE_NUM 1
 
-//draw current board state
-void Board::draw()
+class Board
 {
-	std::cout << std::string(70, '\n');
-	for(int i = 0; i < SPACE_NUM; i++)
-	{
-		std::cout << "Space " << i << "   ";
-		for(int j = 0; j < PIECE_NUM; j++)
-		{
-			//show piece location
-			if(this->playerPieces[j].getPosition() == i)
-			{
-				std::cout << "piece " << j << " is here.   ";
-				
-				//if piece is on a buildling, explain building
-				if(this->BoardSpaces[i]->getSpaceType() == "Building")
-				{
-					this->BoardSpaces[i]->explain();
-				}
-			}
-		}
-		std::cout << "\n"; 
-	}
-}
+private:
+	Space* BoardSpaces[SPACE_NUM];
+	Piece* playerPieces[PIECE_NUM];
+
+public:
+	Board();
+	~Board();
+
+	void move(int index, int moveNum);
+	void update();
+	void draw();
+
+
+};
+
+#endif
