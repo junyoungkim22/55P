5b338020148297ec3ecf4b33a12de83ba06a29db
pkw1012 <pkw1012@postech.ac.kr>
2015-11-15 21:14:25 +0900

    Update ThreePiece.cpp

--- Piece/ThreePiece.cpp
+++ Piece/ThreePiece.cpp
@@ -1,6 +1,6 @@
 #include "threepiece.h"
 
-ThreePiece::ThreePiece(int HP, int position, int team) : Piece(HP, position, team)
+ThreePiece::ThreePiece(int HP, int position, int team, int num) : Piece(HP, position, team, num)
 {
     setMaxHP(300);
 }
