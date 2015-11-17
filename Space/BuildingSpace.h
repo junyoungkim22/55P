149d0cbc72d7abec816d46ea072ffb0442dde415
pkw1012 <pkw1012@postech.ac.kr>
2015-11-17 23:42:50 +0900

    Update BuildingSpace.h

--- Space/BuildingSpace.h
+++ Space/BuildingSpace.h
@@ -1,5 +1,5 @@
-#ifndef BUILDING_SPACE_H
-#define BUILDING_SPACE_H
+#ifndef BUILDINGSPACE_H
+#define BUILDINGSPACE_H
 
 #include "Space.h"
 #include "Piece.h"
@@ -7,17 +7,24 @@
 class BuildingSpace : public Space
 {
 private:
-	std::string explainString;
-	//Player Owner;
-	int tire;
+    std::string explainString;
+    //Player Owner;
+    int tire;
+    int buy_cost, damage_cost, steal_cost;
 
 public:
-	BuildingSpace();
-	void setExplainString(const std::string s){ explainString = s; };
-	void explain(){ std::cout << explainString; };
-	void buy();
-	void take(Piece* p);
-	void damage(Piece* p);
+    BuildingSpace();
+    void setExplainString(const std::string s){ explainString = s; }
+    void explain(){ std::cout << explainString; }
+    void buy();
+    void take(Piece* p);
+    void damage(Piece* p);
+    void setTire(int tire){this->tire = tire;}
+    int getTire(){return tire;}
+    int getBuycost(){return buy_cost;}
+    int getDamagecost(){return damage_cost;}
+    int getStealcost(){return steal_cost;}
 };
 
-#endif
+#endif // BUILDINGSPACE_H
+
