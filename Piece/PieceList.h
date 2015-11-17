8524b3949fd6da26c983342b608ea1240ad36742
pkw1012 <pkw1012@postech.ac.kr>
2015-11-17 23:41:18 +0900

    Update PieceList.h

--- Piece/PieceList.h
+++ Piece/PieceList.h
@@ -2,14 +2,19 @@
 #define PIECELIST_H
 
 #include "piece.h"
+#include "space.h"
+#include "buildingspace.h"
+#include "healingspace.h"
 
 class PieceList
 {
 private:
-    Piece *team1[3], *team2[3];
+    Piece *piece[2][3];
 public:
     PieceList();
     void turn_action(int team, int piece_num, int move_num);
+    void heal_action(int team, int piece_num, HealingSpace *space);
+    void build_action(int team, int piece_num, BuildingSpace *space);
     void capture(int captured_piece, int team);
     void unite(int p1, int p2, int team);
     void die(int piece_num, int team);
