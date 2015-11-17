4746a705d6fb5f363bb9be16bab5e098edc91833
pkw1012 <pkw1012@postech.ac.kr>
2015-11-17 23:41:48 +0900

    Update PieceList.cpp

--- Piece/PieceList.cpp
+++ Piece/PieceList.cpp
@@ -3,43 +3,31 @@
 #include "threepiece.h"
 #include <typeinfo>
 #include <stdlib.h>
+#include <iostream>
 
 PieceList::PieceList()
 {
     for(int i=0; i<3; i++)
     {
-        team1[i] = new Piece(100, 0, 0, i);
-        team2[i] = new Piece(100, 0, 1, i);
+        piece[0][i] = new Piece(100, 0, 0, i);
+        piece[1][i] = new Piece(100, 0, 1, i);
     }
 }
 
+//윷의 값을 받아서 말을 움직이는 과정(잡거나 말을 합치는 경우도 포함)
 void PieceList::turn_action(int team, int piece_num , int move_num)
 {
     int position, check = 0, i;
 
-    if(team == 0)
-        position = team1[piece_num]->move(move_num);
-    else
-        position = team2[piece_num]->move(move_num);
-
+    position = piece[team][piece_num]->move(move_num);
 
     for(i=0; i<3; i++)
     {
-        if(position == team1[i]->getPosition() && (team != 0 || piece_num != i))
-        {
-            if(team == 0)
-                check = 1;
-            else
-                check = 2;
-        }
+        if(position == piece[team][i]->getPosition() && i != piece_num)
+            check = 1;
 
-        else if(position == team2[i]->getPosition() && (team != 1 || piece_num != i))
-        {
-            if(team == 1)
-                check = 2;
-            else
-                check = 1;
-        }
+        else if(position == piece[!team][i]->getPosition())
+            check = 2;
 
         if(check != 0)
             break;
@@ -51,6 +39,55 @@ void PieceList::turn_action(int team, int piece_num , int move_num)
         capture(i, !team);
 }
 
+//healing space에 도착했을때
+void PieceList::heal_action(int team, int piece_num, HealingSpace *space)
+{
+    int heal_num = space->getheal();
+
+    piece[team][piece_num]->get_heal(heal_num);
+}
+
+//평번한 칸에 도착했을 때
+void PieceList::build_action(int team, int piece_num, BuildingSpace *space)
+{
+    bool death_check;
+    int owner = space->getTire();
+
+    if(owner == team)
+        return;
+
+    else if(owner == !team)
+    {
+        bool steal;
+
+        death_check = piece[team][piece_num]->get_damage(space->getDamagecost());
+        if(death_check == true)
+        {
+            die(piece_num, team);
+            return;
+        }
+
+        if(piece[team][piece_num]->getHP() > space->getStealcost())
+        {
+            if(steal == 1)
+                piece[team][piece_num]->get_damage(space->getStealcost());
+            space->setTire(team);
+        }
+    }
+
+    else
+    {
+        bool buy;
+
+        if(piece[team][piece_num]->getHP() > space->getBuycost())
+        {
+            if(buy == 1)
+                piece[team][piece_num]->get_damage(space->getBuycost());
+            space->setTire(team);
+        }
+    }
+}
+
 void PieceList::capture(int captured_piece, int team)
 {
     die(captured_piece, team);
@@ -58,85 +95,45 @@ void PieceList::capture(int captured_piece, int team)
 
 void PieceList::unite(int p1, int p2, int team)
 {
-    int HP, position;
+    int HP = piece[team][p1]->getHP() + piece[team][p2]->getHP();
+    int position = piece[team][p1]->getPosition();
 
-    if(team == 0)
+    if(typeid(*piece[team][p1]) == typeid(TwoPiece) || typeid(*piece[team][p2]) == typeid(TwoPiece))
     {
-        HP = team1[p1]->getHP() + team1[p1]->getHP();
-        position = team1[p1]->getPosition();
+        delete piece[team][p1];
+        delete piece[team][p2];
 
-        delete team1[p1];
-        delete team1[p2];
-
-        if(typeid(*team1[p1]) == typeid(TwoPiece) || typeid(*team1[p2]) == typeid(TwoPiece))
-            team1[p1] = new ThreePiece(HP, position, team, p1);
-        else
-            team1[p1] = new TwoPiece(HP, position, team, p1);
+        piece[team][p1] = new ThreePiece(HP, position, team, p1);
     }
-
     else
     {
-        HP = team2[p1]->getHP() + team2[p1]->getHP();
-        position = team2[p1]->getPosition();
+        delete piece[team][p1];
+        delete piece[team][p2];
 
-        delete team2[p1];
-        delete team2[p2];
-
-        if(typeid(*team2[p1]) == typeid(TwoPiece) || typeid(*team2[p2]) == typeid(TwoPiece))
-            team2[p1] = new ThreePiece(HP, position, team, p1);
-        else
-            team2[p1] = new TwoPiece(HP, position, team, p1);
+        piece[team][p1] = new TwoPiece(HP, position, team, p1);
     }
-
 }
 
 void PieceList::die(int piece_num, int team)
 {
-    if(team == 0)
+    if(typeid(*piece[team][piece_num]) == typeid(Piece))
     {
-        if(typeid(*team1[piece_num]) == typeid(Piece))
-        {
-            delete team1[piece_num];
-            team1[piece_num] = new Piece(100, 0, team, piece_num);
-        }
+        delete piece[team][piece_num];
+        piece[team][piece_num] = new Piece(100, 0, team, piece_num);
+    }
 
-        else if(typeid(*team1[piece_num]) == typeid(TwoPiece))
-        {
-            delete team1[piece_num];
-            for(int i=0; i<3; i++)
-                if(team1[i] == NULL)
-                    team1[i] = new Piece(100, 0, team, i);
-        }
-
-        else
-        {
-            delete team1[piece_num];
-            for(int i=0; i<3; i++)
-                team1[i] = new Piece(100, 0, team, i);
-        }
+    else  if(typeid(*piece[team][piece_num]) == typeid(TwoPiece))
+    {
+        delete piece[team][piece_num];
+        for(int i=0; i<3; i++)
+            if(piece[team][i] == NULL)
+                piece[team][i] = new Piece(100, 0, team, i);
     }
 
     else
     {
-        if(typeid(*team2[piece_num]) == typeid(Piece))
-        {
-            delete team2[piece_num];
-            team2[piece_num] = new Piece(100, 0, team, piece_num);
-        }
-
-        else if(typeid(*team2[piece_num]) == typeid(TwoPiece))
-        {
-            delete team2[piece_num];
-            for(int i=0; i<3; i++)
-                if(team2[i] == NULL)
-                    team2[i] = new Piece(100, 0, team, i);
-        }
-
-        else
-        {
-            delete team2[piece_num];
-            for(int i=0; i<3; i++)
-                team2[i] = new Piece(100, 0, team, i);
-        }
+        delete piece[team][piece_num];
+        for(int i=0; i<3; i++)
+            piece[team][i] = new Piece(100, 0, team, i);
     }
 }
